b196418197c81cda1bb06e45f4d38588e1bf2933
marcosviana91 <marcos.viana.91@gmail.com>
2024-04-28 12:42:35 -0300

    REgress

--- mysite/tests/models/test_post.py
+++ mysite/tests/models/test_post.py
@@ -8,6 +8,6 @@ def post_published():
     return PostFactory(title='pytest with factory')
 
 
-@pytest.mark.django_db
+@pytest.mark.djando_db
 def test_create_published_post(post_published):
     assert post_published.title == 'pytest with factory'
